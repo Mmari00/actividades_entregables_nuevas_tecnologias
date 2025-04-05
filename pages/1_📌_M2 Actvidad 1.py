87d709dcca07a8fb719daee503d9f311fbd1e2af
Mmari00 <1software.files1@gmail.com>
2025-04-04 21:13:33 -0500

    Creación de los dataframes, actividad #1

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 # Configuración de la página
 st.set_page_config(   
     page_icon="📌",
@@ -16,14 +16,36 @@ tipos de datos, operadores, y las estructuras de datos más utilizadas como list
 diccionarios y conjuntos.
 """)
 
-st.header("Objetivos de aprendizaje")
+st.header("Actividad #1 - Creación de DataFrames")
 
 st.markdown("""
-- Comprender los tipos de datos básicos en Python
-- Aprender a utilizar variables y operadores
-- Dominar las estructuras de datos fundamentales
-- Aplicar estos conocimientos en ejemplos prácticos
+En esta actividad realizaré 2 Dataframes, uno de un diccionario y otro de una lista de diccionarios.
+El primer DataFrame contendrá información sobre libros, incluyendo el título, autor, año de publicación y género, sobre el segundo mostrará información sobre ciudades, incluyendo el nombre, población y país.
 """)
 
-st.header("Solución")
+libros = {
+    "Título del libro": ["El Principito", "Don Quijote de la Mancha", "Cien años de soledad", "Los Juegos del Hambre"],
+    "Autor": ["Antoine de Saint-Exupéry", "Miguel de Cervantes", "Gabriel García Márquez", "Suzanne Collins"],
+    "Año de publicación": [1943, 1605, 1967, 2008],
+    "Género": ["Fábula, Fantasía", "Novela", "Realismo mágico", "Ciencia ficción"],
+}
 
+dataframe_libros = pd.DataFrame(libros)
+
+st.write("DataFrame de libros:")
+st.dataframe(dataframe_libros)
+
+
+st.expander("Mostrar DataFrame de ciudades", expanded=True)
+
+
+ciudades =[
+    {"Nombre": "Medellín", "Población": 58964852, "País": "Colombia"},
+    {"Nombre": "Madrid", "Población": 6589745, "País": "España"},
+    {"Nombre": "Seul", "Población": 35621000, "País": "Corea del Sur"},
+]
+
+dataframe_ciudades = pd.DataFrame(ciudades)
+
+st.write("DataFrame de ciudades:")
+st.dataframe(dataframe_ciudades)
