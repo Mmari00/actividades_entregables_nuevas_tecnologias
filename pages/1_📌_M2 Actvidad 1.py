b8a31e08c60668f113e84ffd9667ed2c4f969db1
Mmari00 <1software.files1@gmail.com>
2025-04-14 18:43:19 -0500

    12 actividades de la Actividad 1 listas

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -1,4 +1,5 @@
 
+import os
 import streamlit as st
 import pandas as pd
 import sqlite3 
@@ -7,6 +8,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
 st.set_page_config(   
     page_icon="📌",
@@ -169,12 +171,39 @@ st.dataframe(dataframe_numpy)
 
 
 
-st.subheader("DataFrame de Firebase: ")
-ruta_firebase = 'proyecto.json'
 
+
+
+load_dotenv()
+type = os.environ.get("TYPE")
+project_id = os.environ.get("PROJECT_ID")
+private_key_id = os.environ.get("PRIVATE_KEY_ID")
+private_key = os.environ.get("PRIVATE_KEY").replace('\\n', '\n') # Reemplazar los saltos de línea escapados
+client_email = os.environ.get("CLIENT_EMAIL")
+client_id = os.environ.get("CLIENT_ID")
+auth_uri = os.environ.get("AUTH_URI")
+token_uri = os.environ.get("TOKEN_URI")
+auth_provider_x509_cert_url = os.environ.get("AUTH_PROVIDER_X509_CERT_URL")
+client_x509_cert_url = os.environ.get("CLIENT_X509_CERT_URL")
+universe_domain = os.environ.get("UNIVERSE_DOMAIN")
+
+
+st.subheader("DataFrame de Firebase: ")
 if not firebase_admin._apps:
-    credenciales = credentials.Certificate(ruta_firebase)
-    firebase_admin.initialize_app(credenciales)
+    cred = credentials.Certificate({
+        "type": type,
+        "project_id": project_id,
+        "private_key_id": private_key_id,
+        "private_key": private_key,
+        "client_email": client_email,
+        "client_id": client_id,
+        "auth_uri": auth_uri,
+        "token_uri": token_uri,
+        "auth_provider_x509_cert_url": auth_provider_x509_cert_url,
+        "client_x509_cert_url": client_x509_cert_url,
+        "universe_domain": universe_domain,
+    })
+    firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 coleccion = 'usuarios'
