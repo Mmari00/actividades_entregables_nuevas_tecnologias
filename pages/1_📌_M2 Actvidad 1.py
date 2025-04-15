7ab2771ff0ddc672481f275b401ebec8fe9c86aa
Mmari00 <1software.files1@gmail.com>
2025-04-14 21:06:56 -0500

    ...

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import sqlite3 
 import numpy as np
-import firebase_admin
+# import firebase_admin
 # from firebase_admin import credentials
 # from firebase_admin import firestore
 # from pymongo import MongoClient
